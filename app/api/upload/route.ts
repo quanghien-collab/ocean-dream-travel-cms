ac011811824b6ea27446c9bb44eb99e7d4d87523
quanghien-collab <HIENCQ@vuongDP>
2026-01-13 14:15:34 +0700

    Fix upload api supabase

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -18,21 +18,28 @@ export async function POST(req: Request) {
     const ext = file.name.split(".").pop();
     const fileName = `tour-${Date.now()}.${ext}`;
 
+    // convert File → Buffer
+    const arrayBuffer = await file.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+
     const { error } = await supabase.storage
       .from("images")
-      .upload(fileName, file, {
+      .upload(fileName, buffer, {
+        contentType: file.type,
         cacheControl: "3600",
         upsert: false,
-        contentType: file.type,
       });
 
     if (error) {
       return NextResponse.json({ error: error.message }, { status: 500 });
     }
 
-    const { data } = supabase.storage.from("images").getPublicUrl(fileName);
+    const { data } = supabase.storage
+      .from("images")
+      .getPublicUrl(fileName);
 
     return NextResponse.json({ url: data.publicUrl });
+
   } catch (err: any) {
     return NextResponse.json({ error: err.message }, { status: 500 });
   }
